b702bd1a5e87ea191627677f8ee7edd98d560a42
anamerasul <anamerasul@gmail.com>
2022-05-25 23:57:38 +0600

    footer add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./pages/Shared/Header/Header";
+import Footer from "./pages/Shared/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Footer></Footer>
     </div>
   );
 }
