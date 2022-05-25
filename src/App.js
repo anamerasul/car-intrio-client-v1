5f2ac3989a917f41ed8873b469c63b7a5186320b
anamerasul <anamerasul@gmail.com>
2022-05-25 23:43:37 +0600

    IMplement header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./pages/Shared/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">WELL COME TO CARINTRIO</h1>
+      <Header />
     </div>
   );
 }
