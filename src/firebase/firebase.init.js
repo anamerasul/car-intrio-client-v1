abac1b352a8f3682043dafb827701b2924e7a344
anamerasul <anamerasul@gmail.com>
2022-05-25 17:20:23 +0600

    firebase add

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4yNCg3kmRcJbzKX3yigWf-FHAL4qi2A4",
+  authDomain: "car-intrio.firebaseapp.com",
+  projectId: "car-intrio",
+  storageBucket: "car-intrio.appspot.com",
+  messagingSenderId: "783285673354",
+  appId: "1:783285673354:web:9dd9152fc746bc2749bc73"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
